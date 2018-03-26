6d690ac3041e32e228ee85dbada325471f039607
Adrian Sims <adriansims62781@gmail.com>
2018-03-26 09:13:52 +0100

    Add project files.

---
+++ GetChilternFares/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetChilternFares
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var flowidMap = new HashSet<int>();
+                foreach (var line in File.ReadAllLines("s:\\rjfaf782.ffl"))
+                {
+                    if (line.Length <= 2 || line[0] != 'R')
+                    {
+                        continue;
+                    }
+                    if (line[1] == 'F')
+                    {
+                        if (line.Substring(36, 3) == "NCH")
+                        {
+                            var flowid = Convert.ToInt32(line.Substring(42));
+                            flowidMap.Add(flowid);
+                        }
+                    }
+                    else if (line[1] == 'T')
+                    {
+                        var flowid = Convert.ToInt32(line.Substring(2, 7));
+                        var rescode = line.Substring(20, 2);
+                        if (rescode != "  " && flowidMap.Contains(flowid))
+                        {
+                            Console.WriteLine($"{rescode}");   
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add GetChilternFares/Program.cs
+git add GetChilternFares/App.config
+git add GetChilternFares/Properties/AssemblyInfo.cs
+git add GetChilternFares/GetChilternFares.csproj
+git add GetChilternFares.sln
+git add .gitignore
