babaff630e81de8eb54132fb633bb769e82d2349
Adrian Sims <adriansims62781@gmail.com>
2018-04-08 14:45:53 +0100

    Write restriction usage to file.

--- GetChilternFares/DictUtils.cs
+++ GetChilternFares/DictUtils.cs
@@ -1,4 +1,23 @@
-﻿namespace GetChilternFares
+﻿// Copyright (c) Adrian Sims 2018
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+namespace GetChilternFares
 {
     using System.Collections.Generic;
 
@@ -13,6 +32,16 @@
         /// <param name="key">Dictionary key of the List to add to</param>
         /// <param name="listEntry">Value to add to the List</param>
         public static void AddEntryToList<T1, T2>(Dictionary<T1, List<T2>> d, T1 key, T2 listEntry)
+        {
+            if (!d.TryGetValue(key, out var list))
+            {
+                list = new List<T2>();
+                d.Add(key, list);
+            }
+
+            list.Add(listEntry);
+        }
+        public static void AddEntryToList<T1, T2>(IDictionary<T1, List<T2>> d, T1 key, T2 listEntry)
         {
             if (!d.TryGetValue(key, out var list))
             {
--- GetChilternFares/Program.cs
+++ GetChilternFares/Program.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿// Copyright (c) Adrian Sims 2018
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,11 +36,24 @@ namespace GetChilternFares
             try
             {
                 var setnum = 796;
+
+                // list of all flow Ids for the TOC in question:
                 var flowidMap = new HashSet<int>();
+
+                // list of all 
                 var rescodeMap = new HashSet<string>();
+
+                // flow id -> RF line:
+                var flowIdToFlowMap = new Dictionary<int, string>();
+
+                // restriction code to a list of F records:
+                var resCodeToFlowList = new SortedDictionary<string, List<string>>();
+
                 foreach (var line in File.ReadAllLines($"s:\\rjfaf{setnum:D3}.ffl"))
                 {
-                    if (line.Length <= 2 || line[0] != 'R')
+                    var flow = line.Substring(2, 8);
+
+                    if (line.Length <= 2 || line[0] != 'R' || !"QRST01234567889".Contains(flow[0]) || !"QRST01234567889".Contains(flow[4]))
                     {
                         continue;
                     }
@@ -30,6 +63,7 @@ namespace GetChilternFares
                         {
                             var flowid = Convert.ToInt32(line.Substring(42));
                             flowidMap.Add(flowid);
+                            flowIdToFlowMap.Add(flowid, line);
                         }
                     }
                     else if (line[1] == 'T')
@@ -39,13 +73,24 @@ namespace GetChilternFares
                         if (rescode != "  " && flowidMap.Contains(flowid))
                         {
                             rescodeMap.Add(rescode);
-                            if (rescode == "B5")
-                            {
-                                Console.WriteLine();
-                            }
+                            DictUtils.AddEntryToList(resCodeToFlowList, rescode , flowIdToFlowMap[flowid]);
                         }
                     }
                 }
+
+                var outFilename = "rescode-usage.txt";
+                using (var str = new StreamWriter(outFilename))
+                {
+                    foreach (var entry in resCodeToFlowList)
+                    {
+                        str.WriteLine($"{entry.Key}:");
+                        foreach (var flow in entry.Value)
+                        {
+                            str.WriteLine($"{flow}");
+                        }
+                    }
+                }
+
 
                 var dateMap = new Dictionary<string, List<(DateTime startDate, DateTime endDate, string days)>>();
                 foreach (var line in File.ReadAllLines($"s:\\rjfaf{setnum:D3}.rst"))
@@ -54,6 +99,7 @@ namespace GetChilternFares
                     {
                         continue;
                     }
+
                     if (line[1] == 'H' && line[2] == 'D' && line[3] == 'C')
                     {
                         var rescode = line.Substring(4, 2);
@@ -78,16 +124,19 @@ namespace GetChilternFares
                         }
                     }
                 }
-                foreach (var entry in dateMap)
+                foreach (var entry in dateMap.Where(x=>x.Value.Count() > 0))
                 {
-                    Console.WriteLine($"{entry.Key}");
+                    var restrictionCode = entry.Key;
+                    var dateRestrictionList = entry.Value;
+
+                    Console.WriteLine($"{restrictionCode}");
 
                     var datelist = new List<DateTime>();
 
                     for (var date = new DateTime(2018, 1, 1); date.Year == 2018; date = date.AddDays(1))
                     {
                         bool unrestricted = true;
-                        foreach (var dateRange in entry.Value)
+                        foreach (var dateRange in dateRestrictionList)
                         {
                             var euroDayofWeek = (int) (7 + (date.DayOfWeek - 1)) % 7;
                             if (dateRange.days[euroDayofWeek] == 'N')
